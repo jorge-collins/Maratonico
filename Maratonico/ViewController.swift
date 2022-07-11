8a6d25c41c4505123334bb3d398b43106131c8d0
jorge-collins <jorge.collins@gmail.com>
2022-07-11 14:38:41 -0600

    Initial Commit

---
+++ Maratonico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maratonico
+//
+//  Created by Jorge Collins Gómez on 11/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
